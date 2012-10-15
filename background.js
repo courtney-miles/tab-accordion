9508ad96a9d0b1eeb422dd9f9b747ab7d411f27a
Courtney Miles <courtney@milesasylum.com>
2012-10-15 19:03:28 +1000

    Rewrite of JS code and added behaviour when moving tabs.

--- background.js
+++ background.js
@@ -1,27 +1,66 @@
-var lastSelected;
+var tabAccordion = new function() {
+    this.prevFocusedTab = null;
+    this.focusedGroup = null;
+
+    this.constructor = function() {
+        this.prevFocusedTab = opera.extension.tabs.getFocused();
+        this.focusedGroup = opera.extension.tabs.getFocused().tabGroup;
+        this.toggleExpansion(this.focusedGroup);
+    }
+
+    this.toggleExpansion = function(tabGroup) {
+        var groups = opera.extension.tabGroups.getAll();
+
+        groups.forEach(function(g) {
+            var newState = true;
+            if (tabGroup == g)
+                newState = false;
+            if (newState != g.collapsed)
+                g.update({ collapsed: newState });
+        });
+    }
+
+    this.snapBack = function(tab) {
+        var sel = opera.extension.tabs.getSelected();
+
+        // if the selected tab is the created tab, the tab is being opened in the foreground, so use the last selected tab
+        if (sel.id == tab.id)
+            sel = this.prevFocusedTab;
+
+        if (!!sel.tabGroup) {
+            sel.tabGroup.insert(e.tab);
+        }
+    }
+
+    return this.constructor();
+}
 
 opera.extension.tabs.onfocus = function(e) {
-  var groups = opera.extension.tabGroups.getAll();
-  var selected = opera.extension.tabs.getSelected().tabGroup;
-  groups.forEach(function(g) {
-      var newState = true;
-      if (selected == g)
-          newState = false;
-      if (newState != g.collapsed)
-          g.update({ collapsed: newState });
-  });
-  
-  lastSelected = selected;
+    tabAccordion.toggleExpansion(e.tab.tabGroup);
+    tabAccordion.prevFocusedTab = e.tab;
+
+    console.log(widget.preferences['snapBackTab']);
+    console.log(widget.preferences.snapBackTab);
 }
 
 opera.extension.tabs.oncreate = function(e) {
-    var sel = opera.extension.tabs.getSelected();
-
-    // if the selected tab is the created tab, the tab is being opened in the foreground, so use the last selected tab
-    if (sel.id == e.tab.id)
-        sel = lastSelected; // lastSelected would need to be maintained elsewhere
-
-    try {
-        sel.tabGroup.insert(e.tab);
-    } catch (f) {}
+    tabAccordion.snapBack(e.tab);
 }
+
+opera.extension.tabs.onmove = function(e) {
+    var tabMoved = e.tab;
+    if (e.tab.focused) {
+        if (!!tabMoved.tagGroup) console.log(tabMoved.tabGroup.collapsed);
+        tabAccordion.toggleExpansion(tabMoved.tabGroup);
+        if (!!tabMoved.tagGroup) console.log(tabMoved.tabGroup.collapsed);
+    }
+}
+
+opera.extension.tabs.onclose = function(e) {
+    //tabClosed.tabGroup.focus();
+    console.log(e);
+}
+
+//opera.extension.tabGroups.oncreate = function (e) {
+//    e.tabGroup.update({collapsed : false});
+//}
